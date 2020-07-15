7efa446d85db81e4698aabc8a0f0a8251a4f6e2b
Glen Pierce <glen.piercev@gmail.com>
2020-07-14 19:51:06 -0700

    initial commit

---
+++ Assets/BoidController.cs
@@ -0,0 +1,34 @@
+﻿using System.Drawing;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class BoidController : MonoBehaviour {
+
+    public static float visionRadius = 4.0f;
+    public static float viewAngle = 270.0f;
+    public bool selected = false;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        Collider[] hitColliders = Physics.OverlapSphere(gameObject.transform.position, visionRadius);
+        if (Application.IsPlaying(gameObject)) {
+            // Play logic
+        } else {
+            Color color;
+            if(selected) {
+                color = Color.red;
+            } else {
+                color = Color.green;
+            }
+            foreach (var hitCollider in hitColliders) {
+                if(gameObject.transform.position != hitCollider.transform.position)
+                    Debug.DrawLine(gameObject.transform.position, hitCollider.gameObject.transform.position, color);
+            }
+        }
+    }
+}
---
+++ Assets/Editor/BoidInspector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteAlways]
+public class BoidInspector : Editor {
+    void Start() {
+        // if (Application.IsPlaying(gameObject)) {
+        //     // Play logic
+        // } else {
+        //     // Editor logic
+        // }
+    }
+
+    void Update() {
+        
+    }
+}
